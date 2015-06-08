15ba598e174121c52b839593a16fe6b58736804f
SpaxMK <spasa.stojssic@gmail.com>
2015-06-08 13:19:01 +0200

    Create FormAbout.cs

---
+++ FormAbout.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Moj_Broj
+{
+    public partial class frmAbout : Form
+    {
+        public frmAbout()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+
+
+        }
+
+        private void frmAbout_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        
+    }
+}
